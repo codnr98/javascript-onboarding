f708192b1cb24b0e683f1e63c057aa0c0ac4b3f4
codnr <wnd75913@gmail.com>
2022-10-27 17:03:41 +0900

    feat<problem1>: 점수 계산함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,14 @@
 function problem1(pobi, crong) {
-  var answer;
+  //점수 계산기
+  function calculator(page, operator) {
+    return page
+      .toString()
+      .split("")
+      .map((x) => +x)
+      .reduce((previous, current) =>
+        operator ? (previous += current) : (previous *= current)
+      );
+  }
   return answer;
 }
 
