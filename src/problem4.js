ceec0275942d247536e24234492a5097265b51b6
codnr <wnd75913@gmail.com>
2022-11-03 22:21:05 +0900

    feat<problem4>: 문자를 청개구리어로 전환하여 배열에 넣어주는 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,11 @@
 function problem4(word) {
-  var answer;
+  var answer = [];
+
+  function convertFrogLang(el, A, Z) {
+    el !== " "
+      ? answer.push(String.fromCharCode(Z - (el.charCodeAt(0) - A)))
+      : answer.push(" ");
+  }
   return answer;
 }
 
