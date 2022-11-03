9263dac2c2978eec8778109b6ad0bcd2ff438014
codnr <wnd75913@gmail.com>
2022-11-03 12:03:43 +0900

    feat<problem3>: 배열의 값들을 받아올 수 있는 iterator array 추가

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,11 @@ function problem3(number) {
   var answer = 0;
   let numArray = Array.from(Array(number + 1).keys()).map((x) => x.toString());
 
+  function arrSearcher(arr) {
+    arr.forEach((element) => {
+      search369(element);
+    });
+  }
   return answer;
 }
 
