6f3c6832d73d70002417e62efc2a4fa7fd291d64
codnr <wnd75913@gmail.com>
2022-11-03 12:05:43 +0900

    feat<problem3>: 받아온 요소에서 3, 6, 9의 값이 포함되었다면 박수횟수를 누산하는 함수 추가

--- src/problem3.js
+++ src/problem3.js
@@ -2,11 +2,20 @@ function problem3(number) {
   var answer = 0;
   let numArray = Array.from(Array(number + 1).keys()).map((x) => x.toString());
 
+  function search369(item) {
+    item.split("").forEach((x) => {
+      if (x == 3 || x == 6 || x == 9) {
+        answer += 1;
+      }
+    });
+  }
+
   function arrSearcher(arr) {
     arr.forEach((element) => {
       search369(element);
     });
   }
+  arrSearcher(numArray);
   return answer;
 }
 
