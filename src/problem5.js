9a4d6f0feb588b886ac916ebbf362c3a5aed9ed7
codnr <wnd75913@gmail.com>
2022-11-12 14:40:48 +0900

    feat<problem5>: 입력값을 차감하고 화폐단위를 변경해주는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -10,6 +10,26 @@ function problem5(money) {
     answer.push(0);
     return 0;
   }
+  // calculatePushValue함수의 값을 리턴받아 해당 화폐단위와 곱하여 입력값에 차감한다.
+  // 그 뒤 화폐의 단위를 한단계 아래 단위로 변경한다.
+  function changeUnit(input, cal) {
+    let moneyUnit = 50000;
+    for (i = 0; i < 9; i++) {
+      input -= moneyUnit * cal(input, moneyUnit);
+      if (i < 7) {
+        if (i % 2 !== 0) {
+          moneyUnit /= 2;
+        } else {
+          moneyUnit /= 5;
+        }
+      } else {
+        moneyUnit /= 10;
+      }
+    }
+  }
+
+  changeUnit(money, calculatePushValue);
+
   return answer;
 }
 
