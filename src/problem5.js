03019d2f9403732d103dde31779640abca33323b
codnr <wnd75913@gmail.com>
2022-11-12 14:36:16 +0900

    feat<problem5>: 입력값을 화폐단위로 나누고 그값을 반내림하여 리턴하는 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+
+  // 입력값을 화폐단위로 나누고 필요한 화폐단위의 갯수를 answer에 저장하고 리턴한다.
+  function calculatePushValue(input, unit) {
+    if (input / unit >= 1) {
+      answer.push(Math.floor(input / unit));
+      return Math.floor(input / unit);
+    }
+    answer.push(0);
+    return 0;
+  }
   return answer;
 }
 
